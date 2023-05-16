19b59559276ff33a9ce2fed27716336a8423d20f
utente <utente@2q4za33by8nhu90mpolp>
2023-05-16 12:09:31 +0200

    r43

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,43 +1,42 @@
 import java.util.Scanner;
-
-// Chiamare la classe col proprio cognome
-public class MainClass
-{
-	static Scanner in = new Scanner(System.in);
-	
-	public static void main(String[] args)
-	{
-		while(true)
-		{
-			stampaMenu();
-			int scelta = Integer.parseInt(in.nextLine());
-			switch(scelta)
-			{
-				//aggiungere, togliere casi a seconda delle proprie scelte
-				case 1:
-					//Inserire metodo statico
-				break;
-				case 2:
-					//Inserire metodo statico
-				break;
-				case 3:
-					//Inserire metodo statico
-				break;
-				default:
-					System.out.println("Scelta errata, riprova!");
-
-			}
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		System.out.println("di che marca è il tuo telefono?");
+		String marca=scan.nextLine();
+		System.out.println("che modello è?");
+		String modello=scan.nextLine();
+		System.out.println("qual'è stato il prezzo di lancio?");
+		int pz=scan.nextInt();
+		System.out.println("quanti pollici misura lo schermo?");
+		double pollici=scan.nextDouble();
+		System.out.println("quanta ram ha?");
+		int ram=scan.nextInt();
+		fascistaprezzo(pz);
+		grandezzaschermo(pollici);
+	}
+	public static void fascistaprezzo(int pz) {
+		if(pz>500) {
+			System.out.println("il tuo telefono fa parte della fascia alta di prezzo");
+		}
+		if(pz<500 && pz>200) {
+			System.out.println("il tuo telefono fa parte della fascia media di prezzo");
+		}
+		if(pz<200) {
+			System.out.println("il tuo telefono fa parte della fascia bassa di prezzo");
 		}
 	}
-	static void stampaMenu()
-	{
-		// Modificare il menù secondo le proprie scelte
-		System.out.println("1 - Es n. * - Titolo es. *");
-		System.out.println("2 - Es n. * - Titolo es. *");
-		System.out.println("3 - Es n. * - Titolo es. *");
-		System.out.println("4 - Es n. * - Titolo es. *");
-		System.out.println("5 - Es n. * - Titolo es. *");
+	public static void grandezzaschermo(int pollici) {
+		if(pollici<5) {
+			System.out.println("il tuo telefono è mini");
+		}
+		if(pollici>5 && pollici <7) {
+			System.out.println("il tuo telefono è di media grandezza");
+		}
+		if(pollici>7) {
+			System.out.println("il tuoe telefono è grande");
+	}
 	}
 	
-	// Aggiungere qui i metodi statici relativi agli esercizi scelti
 }
+
